d0e35106a2893b7a92b59b5fc0a5c2020ab52479
umar <umar.hayat@arkhitech.com>
2014-08-13 15:45:44 +0500

    first commit

--- app/overrides/homepage_contents.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path  => "spree/home/index",
-                     :replace => "[data-hook='homepage_products']",
-                     :name          => "homepage_contents",
-                     :partial => "home/homepage_contents")
